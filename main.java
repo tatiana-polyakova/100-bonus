84d57a545c3ec6c4c2a200a95d418a496b6f5af6
tatiana-polyakova <tanch88@mail.ru>
2020-05-15 11:28:07 +0300

    new numbers

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 public class main {
     public static void main(String [] args) {
-        int balance = 300;
-        int refill = 1500;
-        int result = (balance + refill) / 100;
+        int balance = 319;
+        int refill = 1376;
+        float result = (balance + refill) / 100;
         System.out.println(result);
     }
 }
