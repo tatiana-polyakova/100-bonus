6b80dd419dee220930d908a1606834f670998242
tatiana-polyakova <tanch88@mail.ru>
2020-05-15 11:47:22 +0300

    new files

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String [] args) {
         int balance = 319;
         int refill = 1376;
-        float result = (balance + refill) / 100;
+        float result = (balance + refill) * 0.01F;
         System.out.println(result);
     }
 }
