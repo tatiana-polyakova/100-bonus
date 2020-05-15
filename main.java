e788e8367bfc86e46b09096d1a47a4b7c8aad42c
tatiana-polyakova <tanch88@mail.ru>
2020-05-15 11:20:26 +0300

    add files

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String [] args) {
+        int balance = 300;
+        int refill = 1500;
+        int result = (balance + refill) / 100;
+        System.out.println(result);
+    }
+}
